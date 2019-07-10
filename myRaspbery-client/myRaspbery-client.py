ddfff1783392c3553601cca4a92c2bbadcb3b008
lkre <lukasz.res@gmail.com>
2019-07-10 15:33:06 +0200

    client: add variable t

--- myRaspbery-client/myRaspbery-client.py
+++ myRaspbery-client/myRaspbery-client.py
@@ -5,13 +5,15 @@ import time
 import w1thermsensor
 
 myurl = "https://my-raspberry.herokuapp.com/addTemperature"
+t = 60  # how often send data to server
 
 
 def prepareJson(temperature):
-    body = {'temp': temperature, 'date': str(datetime.datetime.now())} #YYYY-MM-DD HH:MM:SS:MS
+    body = {'temp': temperature, 'date': str(datetime.datetime.now())}  # YYYY-MM-DD HH:MM:SS:MS
     jsonData = json.dumps(body)
-    jsonDataAsBytes = jsonData.encode('utf-8')   # needs to be bytes
+    jsonDataAsBytes = jsonData.encode('utf-8')  # needs to be bytes
     return jsonDataAsBytes
+
 
 def prepareRequest(content):
     req = urllib.request.Request(myurl)
@@ -19,9 +21,11 @@ def prepareRequest(content):
     req.add_header('Content-Length', len(content))
     return req
 
+
 def readSensor():
     sensor = w1thermsensor.W1ThermSensor()
     return sensor.get_temperature()
+
 
 def getAverage(array):
     sum = 0;
@@ -29,13 +33,14 @@ def getAverage(array):
         sum += n
     return sum / len(array)
 
+
 while True:
     i = 0;
-    result = [0]*10
-    while i<10:
+    result = [0] * t
+    while i < t:
         result[i] = readSensor()
         time.sleep(1)
-        i+=1
+        i += 1
     newJson = prepareJson(getAverage(result))
     req = prepareRequest(newJson)
     print (newJson)
